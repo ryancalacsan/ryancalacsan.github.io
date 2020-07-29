ed7c643462d44d024bfbf96ecf4577ccf824fa76
Ryan Calacsan <ryancalacsan@gmail.com>
2020-07-29 03:33:20 -0500

    created theme options

--- js/index.js
+++ js/index.js
@@ -42,3 +42,41 @@ $(window)
     });
   })
   .scroll();
+
+let theme = localStorage.getItem('theme');
+
+if (theme == null) {
+  setTheme('light');
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+for (let i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener('click', function () {
+    let mode = this.dataset.mode;
+    console.log('Option clicked:', mode);
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == 'light') {
+    document.getElementById('theme-style').href = 'css/style.css';
+  }
+
+  if (mode == 'pink') {
+    document.getElementById('theme-style').href = 'css/pink.css';
+  }
+
+  if (mode == 'green') {
+    document.getElementById('theme-style').href = 'css/green.css';
+  }
+
+  if (mode == 'purple') {
+    document.getElementById('theme-style').href = 'css/purple.css';
+  }
+
+  localStorage.setItem('theme', mode);
+}
